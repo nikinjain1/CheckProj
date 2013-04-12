80be2ba5fa4be6fdc1c14fb9a24089b688b44953
Nikin <nikin.jain@chronus.com>
2013-04-12 22:44:41 +0530

    Initial Proj

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CheckProj::Application.config.secret_token = '275a12c62bb9c0be0e21f829083e2d6bb68fdc2c9faf24f7bda7cef7f6f1b34ac2187954534a93abce935f16a28e794069e5b61a5661597c9bccd14dd1715505'
