3478faa605f6c31c547550dc7bcafa2e62a6e790
chronusjain <nikin.jain@chronus.com>
2013-04-15 10:37:34 +0530

    Made Version Related Changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,16 @@ CheckProj::Application.routes.draw do
   get "users/new"
 
   resources :users
-  resources :sessions, only: [:new, :create, :destroy]
+  resources :sessions, :only => [:new, :create, :destroy]
 
-  root to: 'static_pages#home'
+  root :to => 'static_pages#home'
 
-  match '/signup', to: 'users#new'
-  match '/signin', to: 'sessions#new'
-  match '/signout', to: 'session#destroy'
+  match '/signup', :to => 'users#new'
+  match '/signin', :to => 'sessions#new'
+  match '/signout', :to => 'session#destroy'
 
-  match '/help', to:'static_pages#help'
-  match '/about', to:'static_pages#about'
+  match '/help', :to => 'static_pages#help'
+  match '/about', :to => 'static_pages#about'
 
   get "static_pages/home"
 
---
+++ equip_ruby.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+#
+# Ubuntu Equip 
+#  Ruby Equip
+# Licence: MIT
+
+
+wget --no-check-certificate https://github.com/aglover/ubuntu-equip/raw/master/equip_base.sh && bash equip_base.sh
+
+sudo apt-get install libssl-dev zlib1g-dev libcurl3-dev libxslt-dev zlib1g-dev libreadline-dev libsasl2-dev -y
+sudo apt-get install gcc g++ linux-headers-generic libsqlite3-dev -y
+
+sudo apt-get install libruby1.9 -y
+sudo wget http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.2-p180.tar.gz
+sudo tar xzf ruby-1.9.2-p180.tar.gz
+cd ruby-1.9.2-p180
+sudo ./configure
+sudo make
+sudo make install
+sudo make install-doc
+
+sudo apt-get install rubygems
+
+sudo gem install bundler 
