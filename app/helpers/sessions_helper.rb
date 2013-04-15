0066fa52b02ca6dbaeb55bc054d9158703b92996
chronusjain <nikin.jain@chronus.com>
2013-04-15 14:19:01 +0530

    User Posts allowed

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,8 +1,17 @@
 module SessionsHelper
 
-	def current_user
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+
+  def store_location
+    session[:return_to] = request.url
+  end
+
+  def current_user
 		@current_user ||= User.find_by_remember_token(cookies[:remember_token])
-	end
+  end
 
 
   def sign_in(user)
@@ -22,4 +31,16 @@ module SessionsHelper
 	def current_user=(user)
 		@current_user = user
 	end
+
+	def current_user?(user)
+		user = current_user
+	end
+
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, :notice => "Please Sign In"
+  end
+end
+
 end
